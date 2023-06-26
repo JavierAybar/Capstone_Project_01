29176ba791a56a7aa237239fc886fc511623d7ee
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-06-25 22:26:10 -0300

    Add mobile version

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+import hamburgerMenu from './hamburger_menu.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  hamburgerMenu('.toolbar_btn', '.toolbar__menu', '.toolbar__menuLink');
+});
