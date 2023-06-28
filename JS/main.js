e65e8e84bb533e66068fc077239e1f6eb857cbc7
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-06-27 23:58:01 -0300

    Fix linters

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,12 @@
 // Hamburger menu  //
 import hamburgerMenu from './hamburger_menu.js';
 
+// Add testimonial section dynamically //
+import testimonialObj from './testimonial_data.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   hamburgerMenu('.toolbar_btn', '.toolbar__menu', '.toolbar__menuLink');
 });
-
-// Add testimonial section dynamically //
-import testimonialObj from './testimonial_data.js';
 
 const speakerContainer = document.querySelector('.speaker__cards');
 
@@ -16,8 +16,7 @@ testimonialObj.forEach((speakerData, i) => {
 
   const classHiden = i > 1 ? 'hiden__speaker' : '';
 
-  speaker.innerHTML =
-    `<img
+  speaker.innerHTML = `<img
     class="speaker__img ${classHiden}"
     src="${speakerData.image}"
     alt="${speakerData.name}"
@@ -26,27 +25,27 @@ testimonialObj.forEach((speakerData, i) => {
     <h3 class="speaker__name">${speakerData.name}</h3>
     <p class="speaker__profession">${speakerData.profession}</p>
     <p class="speaker__description">${speakerData.description}</p>
-  </div>`
+  </div>`;
 
   speakerContainer.appendChild(speaker);
 });
 
 // Funcionality for btn see more //
 const btnSeeMore = document.querySelector('.speaker__btn');
-const arrowUp = document.querySelector('.arrow_down-icon')
+const arrowUp = document.querySelector('.arrow_down-icon');
 const hidenSpeaker = document.querySelectorAll('.hiden__speaker');
 const btnText = document.querySelector('.speaker__btn-span');
-const seeLess = 'See Less'
-const seeMore = 'See More'
+const seeLess = 'See Less';
+const seeMore = 'See More';
 
 btnSeeMore.addEventListener('click', () => {
-   hidenSpeaker.forEach((show) => {show.classList.toggle('hiden__speaker');});
+  hidenSpeaker.forEach((show) => { show.classList.toggle('hiden__speaker'); });
 
   // change content of button //
-   arrowUp.classList.toggle('arrow__up-icon');
-   if (btnText.textContent === 'See More') {
+  arrowUp.classList.toggle('arrow__up-icon');
+  if (btnText.textContent === 'See More') {
     btnText.textContent = seeLess;
-   }else {
+  } else {
     btnText.textContent = seeMore;
-   }
+  }
 });
