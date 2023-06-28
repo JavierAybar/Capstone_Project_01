e22adc19306278591a4cdd279f7ee6374676615a
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-06-27 23:12:28 -0300

    Add HTML dinamically/button funtions

---
+++ JS/testimonial_data.js
@@ -0,0 +1,46 @@
+const testimonialObj = [
+    {
+        name: "John Doe",
+        profession: "Software Developer at TechCo",
+        description:
+            "Thanks to Aybar Academy, I gained the skills and knowledge needed to excel in the software development field. I am now working as a software developer at TechCo, building innovative solutions.",
+        image: "images/speaker_1.svg",
+    },
+    {
+        name: "Jane Smith",
+        profession: "Full Stack Developer at StartupX",
+        description:
+            "Aybar Academy provided me with a comprehensive coding education and helped me become a versatile full stack developer. I am now thriving at StartupX, contributing to exciting projects and shaping the future of technology.",
+        image: "images/speaker_2.svg",
+    },
+    {
+        name: "Alex Johnson",
+        profession: "Frontend Developer at DesignLab",
+        description:
+            "Aybar Academy's focus on frontend development empowered me to create engaging user interfaces and bring designs to life. Now, as a frontend developer at DesignLab, I enjoy crafting seamless digital experiences.",
+        image: "images/speaker_3.svg",
+    },
+    {
+        name: "Sarah Thompson",
+        profession: "Backend Developer at Analytics Corp",
+        description:
+            "Aybar Academy's backend development program equipped me with the skills to build robust and scalable applications. I am now a backend developer at Analytics Corp, working with complex systems and optimizing performance.",
+        image: "images/speaker_4.svg",
+    },
+    {
+        name: "Michael Davis",
+        profession: "Mobile App Developer at Innovate Mobile",
+        description:
+            "Aybar Academy's mobile app development course allowed me to specialize in creating cutting-edge mobile experiences. Now, as a mobile app developer at Innovate Mobile, I contribute to innovative projects and shape the mobile landscape.",
+        image: "images/speaker_5.svg",
+    },
+    {
+        name: "Emily Rodriguez",
+        profession: "UI/UX Designer at Creative Solutions",
+        description:
+            "Aybar Academy's focus on UI/UX design helped me master the art of creating intuitive and visually appealing interfaces. Now, as a UI/UX designer at Creative Solutions, I combine creativity and user-centric design principles to deliver exceptional experiences.",
+        image: "images/speaker_6.svg",
+    },
+];
+
+export default testimonialObj;
